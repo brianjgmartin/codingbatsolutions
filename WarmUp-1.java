62183a3018b77155ea26ad44d1a75b400082cc21
Brian Martin <brian.martin@workday.com>
2014-04-09 11:20:45 +0100

    commit new problen solutions

--- WarmUp-1.java
+++ WarmUp-1.java
@@ -17,3 +17,46 @@ public boolean sleepIn(boolean weekday, boolean vacation) {
 public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
   return(aSmile == bSmile || !aSmile == !bSmile);
  }
+ 
+// Given two int values, return their sum. 
+// Unless the two values are the same, then return double their sum. 
+
+public int sumDouble(int a, int b) {
+  if (a ==b) return 2*(a+b);
+  return (a+b);
+}
+
+// Given an int n, return the absolute difference between n and 21,
+//  except return double the absolute difference if n is over 21. 
+
+ public int diff21(int n) {
+  if(n<=21){
+  return 21-n;
+  }
+  return 2*(n-21);
+}
+
+
+// We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. 
+// We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble. 
+
+public boolean parrotTrouble(boolean talking, int hour) {
+  if((hour<7 || hour>20) && talking == true)return true;
+  return false
+  ;
+  }
+
+  // Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10. 
+
+  public boolean makes10(int a, int b) {
+  return (((a+b) == 10) || (a==10) ||(b==10));
+}
+
+
+// Given an int n, return true if it is 
+// within 10 of 100 or 200. Note: Math.abs(num) 
+// computes the absolute value of a number. 
+
+public boolean nearHundred(int n) {
+return(Math.abs(100-n)<=10 || Math.abs(200-n)<=10);
+  }
