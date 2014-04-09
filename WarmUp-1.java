d52e0eeb8cd9f5a652bc2ff5ebc8de541a86e5d2
Brian Martin <brian.martin@workday.com>
2014-04-09 16:46:54 +0100

    some formatting

--- WarmUp-1.java
+++ WarmUp-1.java
@@ -1,7 +1,7 @@
 //  Brian Martin 09/04/2014
 // These are the solutions to WarmUp-1 www.codingbat.com java problems
 // All methods have being tested and verified
-// This is a hotfiX
+//This is a hotfiXi
 
 // sleepIn
 // The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if 
@@ -21,18 +21,23 @@ public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
 // Unless the two values are the same, then return double their sum. 
 
 public int sumDouble(int a, int b) {
-  if (a ==b) return 2*(a+b);
-  return (a+b);
-}
+  if (a == b) {
+    return 2*(a+b);
+  }
+  else{
+    return (a+b);
+  }
+ }
+
 
 // Given an int n, return the absolute difference between n and 21,
 //  except return double the absolute difference if n is over 21. 
 
  public int diff21(int n) {
   if(n<=21){
-  return 21-n;
+    return 21-n;
   }
-  return 2*(n-21);
+    return 2*(n-21);
 }
 
 
@@ -40,7 +45,9 @@ public int sumDouble(int a, int b) {
 // We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble. 
 
 public boolean parrotTrouble(boolean talking, int hour) {
-  if((hour<7 || hour>20) && talking == true)return true;
+  if((hour<7 || hour>20) && talking == true){
+    return true;
+  }
   return false;
   }
 
@@ -56,22 +63,23 @@ public boolean parrotTrouble(boolean talking, int hour) {
 // computes the absolute value of a number. 
 
 public boolean nearHundred(int n) {
-return(Math.abs(100-n)<=10 || Math.abs(200-n)<=10);
+return(Math.abs(100 - n)<= 10 || Math.abs(200-n)<= 10);
   }
 
 // Given 2 int values, return true if one is negative and one is positive. 
 // Except if the parameter "negative" is true, then return true only if both are negative. 
 
 public boolean posNeg(int a, int b, boolean negative) {
-  return(a<0 && b>0 && negative == false || a>0 && b<0 && negative == false
-  || a<0 && b< 0 && negative == true);
+  return(a < 0 && b > 0 && negative == false ||
+         a > 0 && b < 0 && negative == false ||
+         a < 0 && b < 0 && negative == true);
   }
 
   // Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", 
   // return the string unchanged. Note: use .equals() to compare 2 strings.
 
 public String notString(String str) {
-if(str.length()>=3 && str.substring(0,3).equals("not"))
+if(str.length() >= 3 && str.substring(0,3).equals("not"))
 {
 return str;
 }
@@ -83,7 +91,7 @@ return str;
 
 public String missingChar(String str, int n) {
   String s = str.substring(0,n);
-  String b  =  str.substring (n+1,str.length());
+  String b =  str.substring (n+1,str.length());
   return s+b;
 }
 
@@ -91,8 +99,9 @@ public String missingChar(String str, int n) {
 // Given a string, return a new string where the first and last chars have been exchanged. 
 
 public String frontBack(String str) {
-  if(str.length()<=1) 
-  return str;
+  if(str.length() <= 1) {
+   return str;
+  }
   String first = str.substring(0,1);
   String last = str.substring(str.length()-1);
   String middle = str.substring(1,str.length()-1);
@@ -103,9 +112,11 @@ public String frontBack(String str) {
   // If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front. 
 
 public String front3(String str) {
-  if(str.length()<=3) return str+str+str;
+  if(str.length() <= 3){
+    return str + str + str;
+  }
   String front = str.substring(0,3);
-  return front +front +front;
+  return front + front + front;
 }
 
 
@@ -121,8 +132,8 @@ return last + str + last;
 // Use the % "mod" operator -- see Introduction to Mod 
 
 public boolean or35(int n) {
-  return(n%5 ==0 || n%3 ==0);
-  }
+  return(n%5 == 0 || n%3 == 0);
+}
 
 
 // Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". 
@@ -132,18 +143,20 @@ public String front22(String str) {
   int take = 2;
   if(take> str.length()){
   take = str.length();
-  }
+}
   String front  = str.substring(0,take);
   return front + str + front;
-  }
+}
 
 
 // Given a string, return true if the string starts with "hi" and false otherwise. 
 
 public boolean startHi(String str) {
-if (str.length() < 2) return false;
+if (str.length() < 2) {
+  return false;
+}
  String front =  str.substring(0,2);
- if(front.equals("hi")){
+ if (front.equals("hi")){
  return true;
  }
  else{return false;}
@@ -173,11 +186,11 @@ public boolean hasTeen(int a, int b, int c) {
 //  Otherwise, return the string unchanged. 
 
 public String delDel(String str) {
-  if(str.length()>=4 && str.substring(1,4).equals("del")){
-  return str.charAt(0)+str.substring(4,str.length());
-  }
-  return str;
+  if (str.length()>=4 && str.substring(1,4).equals("del")){
+      return str.charAt(0)+str.substring(4,str.length());
  }
+  return str;
+}
 
 // Return true if the given string begins with "mix",
 //  except the 'm' can be anything, so "pix", "9ix" .. all count. 
@@ -190,19 +203,30 @@ return(str.length()>=3 && str.substring(1,3).equals("ix"));
 //  however include first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".
 
 public String startOz(String str) {
-  if(str.length()>=2 && str.substring(0,2).equals("oz")) return ("oz");
-  else if(str.length()>=1&&str.charAt(0)=='o') {return "o";}
-  else if(str.length()>=2&&str.charAt(1)=='z') {return "z";}
-  else return "";
+  if (str.length()>=2 && str.substring(0,2).equals("oz")) {
+    return ("oz");
+  }
+  else if (str.length() >= 1 && str.charAt(0) == 'o') {
+    return "o";
+  }
+  else if (str.length() >= 2 && str.charAt(1) == 'z') {
+    return "z";
+  }
+  else {
+    return "";
+  }
 }
 
 // Given three int values, a b c, return the largest. 
 
 public int intMax(int a int b, int c){
 	int max = a;
-if(b>a) 
-max =b;
- if (c>max) max =c;
+if (b>a) {
+  max =b;
+}
+if (c>max) {
+  max =c;
+}
 return max;
 }
 
@@ -211,10 +235,10 @@ return max;
 //  Note that Math.abs(n) returns the absolute value of a number.
 
 public int close10(int a, int b) {
-  if(Math.abs(a-10) >Math.abs(b-10)){ 
+  if (Math.abs(a - 10) > Math.abs(b - 10)) { 
   return b;
   }
-  if(Math.abs(a-10)==Math.abs(b-10)){
+  if (Math.abs(a - 10) == Math.abs(b - 10)){
    return 0;
    }
   return a;
@@ -225,19 +249,21 @@ public int close10(int a, int b) {
 // or they are both in the range 40..50 inclusive. 
 
 public boolean in3050(int a, int b) {
-  return((a>=30&&a<=40&&b>=30&&b<=40) || (a>=40&& a<=50 && b>=40 &&b<=50));
+  return(((a >= 30) && (a <= 40) && (b >= 30) && (b <= 40)) || ((a >= 40) && (a <= 50) && (b >= 40) && (b <= 50));
 }
 
 // Return true if the given string contains between 1 and 3 'e' chars. 
 
 public boolean stringE(String str) {
 int count = 0;
-for(int i = 0;i<str.length();i++){
-if (str.charAt(i) =='e') count++;
-}
-if (count ==1 || count == 3){
-return true;
-}
+ for (int i = 0 ; i < str.length() ; i++) {
+   if (str.charAt(i) =='e') {
+    count++;
+    }
+   }
+   if (count ==1 || count == 3){
+  return true;
+ }
 return false;
 }
 
@@ -245,7 +271,7 @@ return false;
 //  Note that the % "mod" operator computes remainders, so 17 % 10 is 7. 
 
 public boolean lastDigit(int a, int b) {
-  return(b%10 ==a%10);
+  return( b %1 0 == a % 10);
 }
 
 // Given a string, return a new string where the last 3 chars are now in upper case.
@@ -255,8 +281,10 @@ public boolean lastDigit(int a, int b) {
 public String endUp(String str) {
 String st;
 String start;
-   if (str.length() <=3) return str.toUpperCase();
-   else{
+   if (str.length() <= 3) {
+    return str.toUpperCase();
+    } 
+   else {
    st = str.substring(str.length()-3,str.length());
    start = str.substring(0,str.length()-3);
    st.toUpperCase();
@@ -270,8 +298,9 @@ String start;
 
 public String everyNth(String str, int n) {
 StringBuilder sb = new StringBuilder();
-for (int i = 0 ;i<str.length() ;i+=n){
-sb.append(str.charAt(i));}
+for (int i = 0 ; i < str.length() ; i += n) {
+sb.append(str.charAt(i));
+   }
 return sb.toString();
   }
 
